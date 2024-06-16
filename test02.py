5339bdfd82acd01a726858dfdb78ef113b9a5454
murozuk <muronta95@gmail.com>
2024-06-16 15:40:50 +0900

    test02
    第2章末課題その２

---
+++ test02.py
@@ -0,0 +1,47 @@
+while True:
+	try:
+		print()
+		print('1: ValueError例外を発生')
+		print('2: IndexError例外を発生')
+		print('3: 例外を発生させない')
+		print('4: 終了')
+		number = int(input('選択してください。: '))
+        # 問①：if文を用いて、以下の画像のように処理されるように記述しましょう。
+		if number == 1:
+			print("↓")
+			print("ValueError")
+			raise ValueError("invalid literal for int() with base 10: 'hoge'",)
+		
+			
+		elif number == 2:
+			print("↓")
+			print("IndexError")
+			raise IndexError('string index out of range',)
+
+		elif number == 3:
+			print("↓")
+			print("例外を発生させませんでした")
+			
+	except ValueError as e:
+		print(e.args)
+		print("↓")
+		print("もう一度選択しましょう")
+
+	except IndexError as d:
+		print(d.args)
+		print("↓")
+		print("もう一度選択しましょう")
+
+		
+    # 問②：else節を用いて、以下の画像のように処理されるように記述しましょう。
+	else:
+		if number == 4:
+			print('↓')
+			print("終了します")
+			print('↓')
+			print('無限ループを終了します')
+			break
+
+		else:
+			print("↓")
+			print("もう一度選択しましょう")
